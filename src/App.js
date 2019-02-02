bae2a2322fcd931906a518167cf52053054828b4
Samweli <smwltwesa6@gmail.com>
2019-02-02 10:56:06 +0300

    fixed cors

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 import './App.css';
+import './assembly.min.css'
+import './assembly.js'
+import loading from './loading.gif'
 
-const CLOUDINARY_UPLOAD_PRESET = 'bmzjbxoq';
-const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/react-cloudinary/upload';
+const UPLOAD_URL = 'http://138.197.5.83:5000/api/v1/detect';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,31 +14,59 @@ export default class App extends React.Component {
 
     this.state = {
       uploadedFile: null,
-      uploadedFileCloudinaryUrl: ''
+      uploadedFileUrl: '',
+      result: null,
+      draggedOver: false,
+      loading: false
     };
   }
 
   onImageDrop(files) {
     this.setState({
-      uploadedFile: files[0]
+      uploadedFile: files[0],
+      draggedOver: false
+    });
+
+    this.setState({
+      loading: true
     });
 
     this.handleImageUpload(files[0]);
   }
 
+  onImageDragOver() {
+    this.setState({
+      draggedOver: true
+    });
+  }
+
+  onImageDragLeave() {
+    this.setState({
+      draggedOver: false
+    });
+  }
+
   handleImageUpload(file) {
-    let upload = request.post(CLOUDINARY_UPLOAD_URL)
-                     .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-                     .field('file', file);
+
+    this.setState({
+      loading: true
+    });
+    let upload = request.post(UPLOAD_URL)
+                     .field('image', file);
 
     upload.end((err, response) => {
       if (err) {
         console.error(err);
       }
+      console.log(response);
 
-      if (response.body.secure_url !== '') {
+      var body = response.body || JSON.parse(response.text)
+
+      if (body.data.output_image_link !== '') {
         this.setState({
-          uploadedFileCloudinaryUrl: response.body.secure_url
+          uploadedFileUrl: body.data.output_image_link,
+          result: body,
+          loading: false
         });
       }
     });
@@ -44,10 +74,30 @@ export default class App extends React.Component {
 
   render() {
     return (
+      <div>
+      <div className="grid mt24 mb60">
+        <div className="col--2">
+        </div>
+        <div className="header col--8 ">
+        <h1> Trash Detection </h1>
+
+        </div>
+        <div className="col--2">
+      </div>
+      </div>
+
+      <div className="grid">
+      <div className="col--2">
+      </div>
+      <div className="col--8 mb12">
+
       <form>
-        <div className="FileUpload">
-          <Dropzone
+        <div className="file_upload">
+          <Dropzone  className={(this.state.draggedOver) ? "dropzone border--dash faint_ground":
+           "dropzone border--dash"}
             onDrop={this.onImageDrop.bind(this)}
+            onDragOver={this.onImageDragOver.bind(this)}
+            onDragLeave={this.onImageDragLeave.bind(this)}
             multiple={false}
             accept="image/*">
             <div>Drop an image or click to select a file to upload.</div>
@@ -55,13 +105,60 @@ export default class App extends React.Component {
         </div>
 
         <div>
-          {this.state.uploadedFileCloudinaryUrl === '' ? null :
-          <div>
-            <p>{this.state.uploadedFile.name}</p>
-            <img src={this.state.uploadedFileCloudinaryUrl} />
-          </div>}
+          
         </div>
       </form>
+      </div>
+       <div className="col--2">
+      </div>
+      </div>
+      <div className="grid">
+        <div className="col submit">
+        {this.state.uploadedFile === null ? null :
+          <div >
+            <h3 className="mb6 text-bold"> Uploaded Image</h3>
+            <img className="col--9" src={this.state.uploadedFile.preview} />
+          </div>}
+        </div>
+        <div className="col--2">
+        {this.state.loading? <img className="mt24" src={loading}/> : ""}
+        {this.state.result === null ? null :
+          <div>
+            <h3 className="content mb6"> <span className="align-center">Content </span></h3>
+            {this.state.result.data.trash_present?
+             <div> 
+             <h4 className="txt-bold center"> TRASH FOUND</h4>
+             <span className="txt-bold"> Approximate Trash area: </span>
+
+             <span>{this.state.result.data.trash_area} </span> <br></br>
+             <span className="txt-bold"> Total time taken:</span>
+             <span> {this.state.result.time} </span>
+             {/*<h4 className="fl"> Result Image url: {this.state.result.data.output_image_link} </h4> */}
+             </div>:
+            <h4 className="txt-bold"> NO TRASH FOUND </h4> }
+          </div>
+        }
+        </div>
+        <div className="col result">
+          {this.state.uploadedFileUrl === '' ? null :
+          <div>
+            <h3 className="mb6 text-bold">Result Image</h3>
+            
+            <img className="col--9" src={this.state.uploadedFileUrl} />
+          </div>}
+        </div>
+
+      </div>
+       <div className="grid mt180 mb60">
+         <div className="col--2">
+         </div>
+         <div className="footer col--8">
+          &copy;  Resilient Dar 
+         </div>
+         <div className="col--2">
+         </div>
+       </div>
+      </div>
     )
   }
 }
